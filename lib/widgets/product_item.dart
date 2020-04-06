46a6fea08efd90bd5a5162178c591c67ea5386f9
unknown <sarjhur@gmail.com>
2020-04-06 17:30:16 +0530

    product overview screen completed

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id,this.title,this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GestureDetector(
+        onTap: ()  {
+          Navigator.of(context).pushNamed(
+            ProductDetailScreen.routeName,
+            arguments: id,
+          );
+        },
+        child: GridTile(
+          child: Image.network(
+            imageUrl, 
+            fit: BoxFit.cover,
+            ),
+            footer: GridTileBar(
+              backgroundColor: Colors.black54,
+              leading: IconButton(
+                icon: Icon(Icons.favorite),
+                iconSize: 20,
+                color: Theme.of(context).accentColor,
+                onPressed: () {},
+                ),
+              trailing: IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.shopping_cart),
+                iconSize: 20,
+                color: Theme.of(context).accentColor,
+                ),
+              title: FittedBox(
+                  fit: BoxFit.fill,
+                  child: Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  ),
+              ),
+            ),
+          
+        ),
+      ),
+    );
+  }
+}
