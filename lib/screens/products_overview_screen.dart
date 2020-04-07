e19f8dea3f3677268163b1b2aadfa5baa0d6a080
unknown <sarjhur@gmail.com>
2020-04-07 17:17:20 +0530

    favourites filter added

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,12 +2,50 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions{
+  Favourites,
+  all,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavourites = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Shop'),),
-      body: ProductsGrid(),
+      appBar: AppBar(
+        title: Text('My Shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue){
+              setState(() {
+                if(selectedValue == FilterOptions.Favourites){
+                  _showOnlyFavourites = true;
+                }
+                else {
+                  _showOnlyFavourites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favourites'), 
+                value: FilterOptions.Favourites
+                ),
+              PopupMenuItem(
+                child: Text('Show All'), 
+                value: FilterOptions.all,
+                ),
+            ],
+          )
+        ],
+        ),
+      body: ProductsGrid(_showOnlyFavourites),
     );
   }
 }
