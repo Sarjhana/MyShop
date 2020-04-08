d4dff3dbc4021d6ec64ad0a28142b02b546bf010
unknown <sarjhur@gmail.com>
2020-04-08 17:36:50 +0530

    dialogbox added

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -28,6 +28,29 @@ class CartItem extends StatelessWidget {
           padding: EdgeInsets.all(20),
           margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                }, 
+                child: Text('No')
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                }, 
+                child: Text('Yes')
+              ),
+            ],
+          )
+          );
+      },
       direction: DismissDirection.endToStart,
       onDismissed: (direction){
         Provider.of<Cart>(context, listen: false).removeItem(productId);
